1b10246749afd2320c084ba568dbb38430c4898d
Denis <deniss.zelenevskis@lighting.com>
2018-03-03 22:43:36 +0100

    Refactoring

--- app/src/main/java/com/denis/githubviewer/data/DbApi.kt
+++
@@ -1,9 +0,0 @@
-package com.denis.githubviewer.data
-
-import com.denis.githubviewer.data.github.GitHubRepo
-
-interface  DbApi {
-    suspend fun eraseOfflineData()
-    suspend fun saveData(data: List<GitHubRepo>)
-    suspend fun getOfflineData(): List<GitHubRepo>
-}
--- app/src/main/java/com/denis/githubviewer/data/realm/RealmRepoItem.kt
+++
@@ -1,7 +0,0 @@
-package com.denis.githubviewer.data.realm
-
-import io.realm.RealmObject
-
-open class RealmRepoItem : RealmObject() {
-    var name: String? = null
-}
