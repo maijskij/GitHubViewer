21bb4968af8ab71fef853f220baebbca5f6a7f8d
Denis <deniss.zelenevskis@lighting.com>
2018-03-06 10:34:37 +0100

    Unit test adjustments

--- app/src/test/java/com/denis/githubviewer/feature/reposlist/ReposListPresenterTest.kt
+++ app/src/test/java/com/denis/githubviewer/feature/reposlist/ReposListPresenterTest.kt
@@ -7,13 +7,14 @@ import com.denis.githubviewer.testutils.MockedCall
 import com.denis.githubviewer.data.db.DbApi
 import com.denis.githubviewer.data.github.GitHubRepo
 import com.denis.githubviewer.data.github.GithubApi
+
 import com.nhaarman.mockito_kotlin.*
 import kotlinx.coroutines.experimental.*
-import kotlinx.coroutines.experimental.android.UI
+
 import org.junit.Before
 import org.junit.Test
 import org.mockito.*
-import org.mockito.Mockito.`when`
+
 
 class ReposListPresenterTest {
 
@@ -32,8 +33,11 @@ class ReposListPresenterTest {
     @Mock
     private lateinit var view: ReposListContract.View
 
-    // Object to test
+    // Class to test
     private lateinit var presenter: ReposListPresenter
+
+    // Class to test
+    private lateinit var customPresenter: ReposListPresenter
 
 
     @Before
@@ -45,7 +49,12 @@ class ReposListPresenterTest {
 
         // Get a reference to the class under test
         presenter = ReposListPresenter(view, dbApi, gitHubApi)
+
+        // Presenter with manually mocked dbApi
+        customPresenter = ReposListPresenter(view, DbApiImpl(), gitHubApi)
+
     }
+
 
     @Test
     fun `Progress view is shown during the data loading and hidden when finished`() = testBlocking {
@@ -80,7 +89,7 @@ class ReposListPresenterTest {
         verify(view, times(1)).addData(DATA_LIST)
     }
 
-    /*
+
     @Test
     fun `Data is saved in DB after successful request to GitHub`() = testBlocking {
 
@@ -92,9 +101,9 @@ class ReposListPresenterTest {
         presenter.loadData()
 
         // DATA_LIST is saved to database
-        verify(dbApi, times(1)).saveData(DATA_LIST)
+        verify(dbApi, times(1)).saveData(DATA_LIST)  //TODO: Second parameter mismatch
     }
-*/
+
 
     @Test
     fun `Presenter enters Last page state when GitHib return empty list`() = testBlocking {
@@ -119,14 +128,11 @@ class ReposListPresenterTest {
         val callMock = MockedCall<List<GitHubRepo>>(exception = Throwable())
         whenever(gitHubApi.getData(com.nhaarman.mockito_kotlin.any(), com.nhaarman.mockito_kotlin.any())).thenReturn(callMock)
 
-        // DbApi is mocked to return DATA_LIST
-        `when`(dbApi.getOfflineData()).thenReturn(DATA_LIST)
-
         // View is mocked to return that it doesn't contain any data in a list yet
         whenever(view.hasNoData()).thenReturn(true)
 
         // When data is requested
-        presenter.loadData()
+        customPresenter.loadData()
 
         // Then proper DATA_LIST is shown in UI
         verify(view, times(1)).addData(DATA_LIST)
@@ -135,35 +141,47 @@ class ReposListPresenterTest {
     }
 
     @Test
-    fun `Data from DB is NOT passed to a view, when GitHibApi throws an Exception, but view ALREADY have data`() = testAsync {
+    fun `Data from DB is NOT passed to a view, when GitHibApi throws an Exception, but view ALREADY have data`() = testBlocking() {
 
         // GitHubApi is mocked to thrown an Exception
         val callMock = MockedCall<List<GitHubRepo>>(exception = Throwable())
         whenever(gitHubApi.getData(any(), any())).thenReturn(callMock)
 
-        // DbApi is mocked to return DATA_LIST
-        whenever(dbApi.getOfflineData()).thenReturn(DATA_LIST)
-
         // View is mocked to return that it already contains data in the list
         whenever(view.hasNoData()).thenReturn(false)
 
-
         // When loading of GitHub repository list is requested
-        presenter.loadData()
+        customPresenter.loadData()
 
         // Then proper DATA_LIST is shown in UI
-        verify(view, never()).addData(DATA_LIST)
+        verify(view, never()).addData(any())
 
         // Then exception message is shown in UI
         verify(view, times(1)).showSnackbar(any())
-
     }
 
-       private fun testAsync(block: suspend CoroutineScope.() -> Unit) {
-        async { block() }
+
+    // Due to suspend functions "whenever" mocking always return null.
+    // Implementing mocked class manually
+    private class DbApiImpl : DbApi {
+        override suspend fun saveData(data: List<GitHubRepo>) {
+            // nothing
+        }
+
+        override suspend fun getOfflineData(): List<GitHubRepo> {
+            return DATA_LIST
+        }
+
+        override suspend fun eraseOfflineData() {
+            // nothing
+        }
     }
 
-    private fun testBlocking(block: suspend () -> Unit) {
-        runBlocking { block() }
+//    private fun testBlocking(block: suspend () -> Unit) {
+//        runBlocking { block() }
+//    }
+
+    private fun testBlocking(block: suspend CoroutineScope.() -> Unit) {
+        runBlocking(Unconfined, block)
     }
 }
